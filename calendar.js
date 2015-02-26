0ce7901bda2e3a8aaca220807e6fd65773164759
Abhi Agarwal <me@abhiagarwal.com>
2015-02-26 18:21:23 -0500

    Making Ethans Changes
    @ethanresnick
    
    Excludes: https://github.com/TechAtNYU/api/pull/35#discussion-diff-24984772

--- calendar.js
+++ calendar.js
@@ -5,7 +5,7 @@ var cal = ical();
  
 cal.setDomain('api.tnyu.org').setName('Events');
  
-request('http://localhost:8000/v1.0/events', function(err, res, body) {
+request({url: 'http://localhost:8000/v1.0/events', rejectUnauthorized: false}, function(err, res, body) {
     if (!err && res.statusCode == 200) {
         var events = JSON.parse(body).events;
         events.forEach(addEvent);
@@ -16,8 +16,8 @@ function addEvent(event) {
     cal.addEvent({
         start: new Date(event.startDateTime),
         end: new Date(event.endDateTime),
-        summary: event.shortTitle || '',
-        description: event.description || '',
+        summary: event.shortTitle || event.title || ('Tech@NYU Event'),
+        description: event.description || event.details,
         url: event.rsvpUrl || ''
     });
 }
