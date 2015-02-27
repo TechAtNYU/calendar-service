45e378a43fa197656c664f9f7f0b028606cda4aa
Ethan <ethan.resnick@gmail.com>
2015-02-27 10:38:58 -0500

    Use production events url

--- calendar.js
+++ calendar.js
@@ -5,7 +5,7 @@ var cal = ical();
  
 cal.setDomain('api.tnyu.org').setName('Events');
  
-request({url: 'http://localhost:8000/v1.0/events', rejectUnauthorized: false}, function(err, res, body) {
+request({url: 'https://api.tnyu.org/v1.0/events', rejectUnauthorized: false}, function(err, res, body) {
     if (!err && res.statusCode == 200) {
         var events = JSON.parse(body).events;
         events.forEach(addEvent);
