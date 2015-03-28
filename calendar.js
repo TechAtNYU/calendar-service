e5b97bd5a58b0cdb3188a270b4fe3c9c46f8fe1d
Ethan <ethan.resnick@gmail.com>
2015-03-28 16:34:43 -0400

    Support event statuses

--- calendar.js
+++ calendar.js
@@ -5,18 +5,28 @@ var cal = ical();
  
 cal.setDomain('api.tnyu.org').setName('Events');
  
-request({url: 'https://api.tnyu.org/v1.0/events', rejectUnauthorized: false}, function(err, res, body) {
+request({url: 'https://api.tnyu.org/v2/events', rejectUnauthorized: false}, function(err, res, body) {
     if (!err && res.statusCode == 200) {
-        var events = JSON.parse(body).events;
+        var events = JSON.parse(body).data;
         events.forEach(addEvent);
     }
 });
 
 function addEvent(event) {
+    var prepend = "";
+    
+    // this is a draft event, so skip it.
+    if(event.links.status.linkage.id === "54837a0ef07bddf3776c79da") return;
+     
+    // if it's a canceled event, add [Canceled] to the title
+    if(event.link.status.linkage.id === "54837a0ec8d83b0e17d7b009") {
+      prepend = "[Canceled] ";
+    }
+    
     cal.addEvent({
         start: new Date(event.startDateTime),
         end: new Date(event.endDateTime),
-        summary: event.shortTitle || event.title || ('Tech@NYU Event'),
+        summary: prepend + (event.shortTitle || event.title || ('Tech@NYU Event')),
         description: event.description || event.details,
         url: event.rsvpUrl || ''
     });
